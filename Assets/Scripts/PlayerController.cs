7088ab4ce279e63266054483685a8dc531c3e1e4
Michael Bandola <24bandola@gmail.com>
2017-06-01 21:45:32 -0400

    roll-a-ball scene 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
 	private Rigidbody rb;
+	private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
 	}
 	void FixedUpdate ()
 	{
@@ -22,7 +26,12 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
 		}
+	}
+	void SetCountText ()
+	{
+	countText.text = "Count: " + count.ToString ();	
 	}
 }
 
